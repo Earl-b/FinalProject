2b7eec1737029460d4f9be74100aa371fdae18c6
T00578223 <lraeb@hotmail.com>
2018-05-22 18:14:55 -0700

    added time picker

--- app/src/main/java/com/example/t00578223/alarm/Add.java
+++ app/src/main/java/com/example/t00578223/alarm/Add.java
@@ -2,12 +2,16 @@ package com.example.t00578223.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class Add extends AppCompatActivity {
+    TimePicker time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add2);
+
+        time = findViewById(R.id.timePicker);
     }
 }
